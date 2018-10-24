1566ca8a14ed2e909d3dc3532b0de594c9e629a8
mqy <1031860108@qq.com>
2018-10-24 10:28:52 +0800

    第一次

---
+++ vantUi/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <tabbar></tabbar>
+      
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    <router-view/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import tabbar from '@/components/tabbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    tabbar
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vantUi/src/components/tabbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tab">
+        <van-tabbar v-model="active">
+        <van-tabbar-item icon="shop">标签</van-tabbar-item>
+        <van-tabbar-item icon="chat" dot>标签</van-tabbar-item>
+        <van-tabbar-item icon="records" info="5">标签</van-tabbar-item>
+        <van-tabbar-item icon="gold-coin" info="20">标签</van-tabbar-item>
+        </van-tabbar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    
+  },
+  data(){
+      
+  }
+}
+</script>
